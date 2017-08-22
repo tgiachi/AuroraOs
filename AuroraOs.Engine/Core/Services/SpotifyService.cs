2e3ce930f9f9f98ee09d198f84d2d59b5d64254b
tommy <squid@stormwind.it>
2017-08-22 18:08:43 +0200

    fix spotify

--- AuroraOs.Engine/Core/Services/SpotifyService.cs
+++ AuroraOs.Engine/Core/Services/SpotifyService.cs
@@ -34,11 +34,34 @@ namespace AuroraOs.Engine.Core.Services
             {
                 var clientId = ConfigManager.Instance.GetConfigValue<SpotifyService>("clientId", "dd204bfd47d64ad09e2f433aca1c918b");
 
-                var webApiFactory = new WebAPIFactory("http://localhost", 8000, clientId,
-                    Scope.UserReadPrivate | Scope.UserReadEmail | Scope.PlaylistReadPrivate | Scope.UserLibraryRead |
-                    Scope.UserReadPrivate | Scope.UserFollowRead | Scope.UserReadBirthdate | Scope.UserTopRead |
-                    Scope.PlaylistReadCollaborative |
-                    Scope.UserReadRecentlyPlayed | Scope.UserReadPlaybackState | Scope.UserModifyPlaybackState);
+                var accessToken = ConfigManager.Instance.GetConfigValue<SpotifyService>("access_token", "");
+
+                if (string.IsNullOrEmpty(accessToken))
+                {
+                    var webApiFactory = new WebAPIFactory("http://localhost", 8000, clientId,
+                        Scope.UserReadPrivate | Scope.UserReadEmail | Scope.PlaylistReadPrivate |
+                        Scope.UserLibraryRead |
+                        Scope.UserReadPrivate | Scope.UserFollowRead | Scope.UserReadBirthdate | Scope.UserTopRead |
+                        Scope.PlaylistReadCollaborative |
+                        Scope.UserReadRecentlyPlayed | Scope.UserReadPlaybackState | Scope.UserModifyPlaybackState);
+
+                    _spotify = await webApiFactory.GetWebApi();
+                    ConfigManager.Instance.SetConfig<SpotifyService>("access_token", _spotify.AccessToken);
+                }
+                else
+                {
+                    _spotify = new SpotifyWebAPI()
+                    {
+                        AccessToken = accessToken,
+                        TokenType = "Bearer",
+                        UseAuth = true
+                    };
+                }
+
+                var userInfo = await _spotify.GetPrivateProfileAsync();
+
+
+                _logger.Info($"Spotify connected as {userInfo.DisplayName}");
             }
             catch (Exception ex)
             {
